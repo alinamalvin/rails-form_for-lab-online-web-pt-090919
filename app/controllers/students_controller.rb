14bf05797459a8b622c1cabad02a72dffbb899c0
Alina Kornilova <alinakornilova78@gmail.com>
2020-02-01 20:34:10 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,44 +1,32 @@
-class StudentsController < ActionController::Base
+class StudentsController < ApplicationController
 
-def show
-		@student = Student.find(params[:id])
-	end
+    def new
+        @student = Student.new
+    end
 
-	def new
-		@student = Student.new
-	end
+    def create
+        @student = Student.new(student_params)
+        @student.save
+        redirect_to student_path(@student)
+    end
 
-	 
-def create
-  @student = Student.new(post_params(:first_name, :last_name)
-  @student.save
-  redirect_to student_path(@student)
-end
+    def show
+        @student = Student.find(params[:id])
+    end
 
-def update
-  @student = Student.find(params[:id])
-  @student.update(student_params(:first_name, :last_name)
-  redirect_to student_path(@student)
-end
+    def edit
+        @student = Student.find(params[:id])
+    end
 
-def edit
-	  @student = Student.find(params[:id])
-	end
-end
- 
-private
- 
-# We pass the permitted fields in as *args;
-# this keeps `post_params` pretty dry while
-# still allowing slightly different behavior
-# depending on the controller action. This
-# should come after the other methods
- 
-def post_params(*args)
-  params.require(:student).permit(*args)
-end
+    def update
+        @student = Student.find(params[:id])
+        @student.update(student_params)
+        redirect_to student_path(@student)
+    end
 
-	
-end
+    private
 
-end
+    def student_params
+        params.require(:student).permit!
+    end
+end 
