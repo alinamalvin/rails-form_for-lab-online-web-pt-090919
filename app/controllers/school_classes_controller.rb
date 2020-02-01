e7235bc325ac8ff60199495f5ce5e6c2e611222c
Alina Kornilova <alinakornilova78@gmail.com>
2020-02-01 20:55:42 +0000

    Automatically backed up by Learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -1,42 +1,33 @@
 class SchoolClassesController < ApplicationController
-  
-	def show
-		@school_class = SchoolClass.find(params[:id])
-	end
 
-	def new
-		@school_class = SchoolClass.new
-	end
+    def new
+        @school_class = SchoolClass.new
+    end
 
-	 
-def create
-  @school_class = SchoolClass.new(school_class_params)
-  @school_class.save
-  redirect_to school_class_path(@school_class)
-end
+    def create
+        @school_class = SchoolClass.new(school_class_params(:title, :room_number))
+        @school_class.save
+        redirect_to school_class_path(@school_class)
+    end
 
-def update
-  @school_class = SchoolClass.find(params[:id])
-  @school_class.update(school_class_params)
-  redirect_to school_class_path(@school_class)
-end
+    def show
+        @school_class = SchoolClass.find(params[:id])
+    end
 
-def edit
-	  @school_class = SchoolClass.find(params[:id])
-	end
-end
- 
+    def edit
+        @school_class = SchoolClass.find(params[:id])
+    end
+
+    def update
+        @school_class = SchoolClass.find(params[:id])
+        @school_class.update(school_class_params(:title))
+        redirect_to school_class_path(@school_class)
+    end
+
 private
- 
-# We pass the permitted fields in as *args;
-# this keeps `post_params` pretty dry while
-# still allowing slightly different behavior
-# depending on the controller action. This
-# should come after the other methods
- 
-def pschool_class_params(*args)
-  params.require(:school_class).permit!
-end
 
-	
+    def school_class_params(*args)
+        params.require(:school_class).permit(*args)
+    end
 
+end 
